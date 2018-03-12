a6fec4ad8d9b57f09170561f7bc0659a15ce2655
Maxim Romashko <md5inj@localhost>
2018-03-12 10:43:02 +0300

    Added a class Screen

---
+++ Screen/Screen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassApp
+{
+    class Screen
+    {
+        public String Firm { get; set; }
+
+        private int _type;
+        public String Type
+        {
+            get
+            {
+                if (this._type == 0) return "String"; else return "enum";
+            }
+
+            set
+            {
+                this._type = Convert.ToInt32(value);
+            }
+        }
+
+        public float Diagonal { get; set; }
+
+        public Screen()
+        {
+            this.Firm = "";
+            this.Type = "0";
+            this.Diagonal = 0;
+        }
+
+        public void Input()
+        {
+            Console.Write("Input manufacter of Screen: "); this.Firm = Console.ReadLine();
+            Console.Write("Input type of screen 0 - String, 1 - enum: "); this.Type = Console.ReadLine();
+            Console.Write("Input diagonal of screen: "); this.Diagonal = (float)Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Firm: { this.Firm }");
+            Console.WriteLine($"Type: { this.Type }");
+            Console.WriteLine($"Diagonal: { this.Diagonal }");
+        }
+
+    }
+}
